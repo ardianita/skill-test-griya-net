575fa91ce0288c6f270ebd252505ac437b8d265f
ardianita <ardianitaf@student.uns.ac.id>
2024-02-10 11:15:35 +0700

    create table user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'username' => 'admin-griyanet',
+                'email' => 'admin.griyanet@gmail.com',
+                'password' => bcrypt('password'),
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
