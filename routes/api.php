f8e47452f3fc2575c4f9e069b5eee877878ea847
ardianita <ardianitaf@student.uns.ac.id>
2024-02-11 22:04:46 +0700

    make routing

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PackageController;
+use App\Http\Controllers\SalesController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,13 +19,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('guest')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 });
 
-Route::prefix('packages')->controller(PackageController::class)->name('package.')->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::post('/', 'store')->name('store');
-    Route::patch('/{idPackage}', 'update')->name('update');
-    Route::delete('/{idPackage}', 'destroy')->name('delete');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('admin')->group(function () {
+        Route::prefix('packages')->controller(PackageController::class)->name('package.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::patch('/{idPackage}', 'update')->name('update');
+            Route::delete('/{idPackage}', 'destroy')->name('delete');
+        });
+
+        Route::prefix('sales')->controller(SalesController::class)->name('sales.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::patch('/{idSales}', 'update')->name('update');
+            Route::delete('/{idSales}', 'destroy')->name('delete');
+        });
+    });
+
+    Route::middleware('sales')->group(function () {
+        Route::prefix('customers')->controller(CustomerController::class)->name('customer.')->group(function () {
+            Route::get('/', 'index')->name('index');
+            Route::post('/', 'store')->name('store');
+            Route::post('/{idCustomer}', 'update')->name('update');
+            Route::delete('/{idCustomer}', 'destroy')->name('delete');
+        });
+    });
+
+
+    Route::post('logout', [AuthController::class, 'logout']);
 });
